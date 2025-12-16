3eaaecd61e85666ef9501f8aaf0a91a7df3b025a
Khrystyna Bash <khrystynabash09@gmail.com>
2025-12-16 15:08:31 +0200

    3 завдання зроблене

---
+++ task_3.py
@@ -0,0 +1,14 @@
+exam_results = [
+    {"student_name": "Анна", "score": 91},
+    {"student_name": "Богдан", "score": 58},
+    {"student_name": "Вікторія", "score": 75},
+    {"student_name": "Григорія", "score": 45}
+]
+passing_score = 60
+
+for student in exam_results:
+    if student ["score"] >= passing_score:
+        student["passed"] = True
+    else:
+        student["passed"] = False
+print(exam_results)
